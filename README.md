e80498874ad05988bb077ca056e43d2ba37b21b1
Dado1513 <dave.caputo93@gmail.com>
2018-05-01 18:08:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Simple skit in arduino to be able to control light based on time
+### obtained through DS231 module, in this case is used to control light of 
+### acquarium.
---
+++ testLightPiero.ino
@@ -0,0 +1,104 @@
+
+#define RELAY1  7 
+#include <DS3231.h>
+#include <Wire.h>
+DS3231 rtc;
+bool h12 = false;
+bool PM = true;
+bool Century = true;
+
+bool set = false;
+int year = 18;
+int month = 5;
+int date = 1;
+int hour = 17;
+int minute = 55;
+int second = 0;
+String accensioneLuce;   //Declare a String variable to hold your name
+bool changeValue = true;
+
+void setup() {
+  Serial.begin(9600);      // turn on Serial Port
+  pinMode(RELAY1, OUTPUT);   
+  Wire.begin();
+
+  if(set){
+    rtc.setClockMode(false);
+    rtc.setYear(year);
+    rtc.setMonth(month);
+    rtc.setDate(20);
+    rtc.setHour(hour);
+    rtc.setMinute(minute);
+    rtc.setSecond(second);
+    rtc.setDoW(2);
+   
+  } 
+}
+ 
+void loop() {
+  
+ while(true){
+      Serial.print(rtc.getYear());
+      Serial.print("-");
+      Serial.print(rtc.getMonth(Century));
+      Serial.print("-");
+      Serial.print(rtc.getDate());
+      Serial.print(" ");
+      Serial.print(rtc.getHour(h12,PM)); //24-hr
+      Serial.print(":");
+      Serial.print(rtc.getMinute());
+      Serial.print(":");
+      Serial.println(rtc.getSecond());
+      Serial.print(rtc.getDoW(), DEC);
+      Serial.println();
+  
+      int hour = int(rtc.getHour(h12,PM)); 
+      
+      if(hour >= 9 && hour <= 20){
+        
+        digitalWrite(RELAY1,0);           // Turns ON Relays 1
+        Serial.println("Light ON");
+      
+      
+      }else{
+        
+        digitalWrite(RELAY1,1);           // Turns ON Relays 1
+        Serial.println("Light OFF");
+        
+      }
+      
+      int dayOfWeek = int(rtc.getDoW());
+      int dayOfMonth = int(rtc.getDate());
+      int month = int(rtc.getMonth(Century));
+      
+      if(changeValue && (month == 3 || month == 10)){
+        
+          if(dayOfWeek == 2 && hour >= 3 && dayOfMonth + 7 < 31 ){
+          
+            switch(month){
+              case 3:
+                rtc.setHour(hour +1 );
+                changeValue = false;
+                break;
+              case 10:
+                rtc.setHour(hour - 1);
+                changeValue = false;
+                break;
+              default:
+                break;
+            }
+        }
+        
+      }
+      
+     if(month == 4 || month == 11){
+      
+        changeValue = true;
+      }
+      
+      
+      delay (60000*10);
+  }
+  
+}
+
